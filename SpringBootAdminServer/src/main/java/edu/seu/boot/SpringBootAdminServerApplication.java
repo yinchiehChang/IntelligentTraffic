d4122a56d9dfb3f46d9fccac6f3d9e4d1155bb74
Yinjie Chang <1654545021@qq.com>
2021-12-23 10:21:16 +0800

    12/23 合并项目

--- SpringBootAdminServer/src/main/java/edu/seu/boot/SpringBootAdminServerApplication.java
+++ SpringBootAdminServer/src/main/java/edu/seu/boot/SpringBootAdminServerApplication.java
@@ -4,16 +4,12 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Spring Boot Admin 可视化监控
- * url: http://localhost:8888/
- */
 @EnableAdminServer
 @SpringBootApplication
-public class SpringBootAdminApplication {
+public class SpringBootAdminServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAdminApplication.class, args);
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
     }
 
 }
